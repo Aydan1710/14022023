7678c37d55ed32e2c4b42026d257e0ca829374e0
Aydansa <aydansa@code.edu.az>
2023-02-14 10:01:09 +0400

    Koment

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // int max = 0;
+            // int[] numbers = {42, 88, 245, };
+
+            // for (int index=0; index<numbers.Length; index++)
+
+            //{
+            //if ( numbers[index] < max) ;
+            //  max = numbers[index];
+
+            //}
+            // Console.WriteLine(max);
+
+
+
+
+
+
+
+            // int[] arr = { 1, 2, -7, -9, -11 };
+            //var count = 0;
+            //for(int i=0; i<arr.Length; i++)
+            //{
+            //  if (arr[i]<0)
+            //{
+            //  count++;
+            //    Console.WriteLine(arr[i]);
+
+            //}
+            // }    if (count == 0)
+            // {
+            //   Console.WriteLine("menfi eded yoxdur");
+            //}
+
+
+
+
+
+
+            //Console.WriteLine("1-ci ededi daxil edin");
+            //string input1 = Console.ReadLine();
+            //var num1 = Convert.ToInt32(input1);
+
+            //Console.WriteLine("2ci ededi daxil edin");
+            //string input2 = Console.ReadLine();
+            //var num2 = Convert.ToInt32(input2);
+
+            //Console.WriteLine("3-cu ededi daxil edin");
+            //string input3 = Console.ReadLine();
+            //var num3 = Convert.ToInt32(input3);
+
+            //if (num1 > num2 && num1 > num3)
+            //{
+              //  Console.WriteLine(num1);
+
+            //}
+
+            //else
+              //  (num2 > num1 && num2 > num1);
+                //    {
+                  //    Console.WriteLine(num2);
+                   //  }
+           //
+           
+             //  else
+
+           
+            //{
+              //  Console.WriteLine(num3);
+            //}
+        
+    }
+}
+
